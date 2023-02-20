160aa448ea706a7b1d20674c9c38695b8dc31579
Fabien MARTY <fabien.marty@gmail.com>
2023-02-20 08:09:32 +0000

    remove useless code

--- standard_structlog/adapter.py
+++ standard_structlog/adapter.py
@@ -10,10 +10,6 @@ from standard_structlog.context import ExecutionContext
 
 
 class ContextVarsAdapter(daiquiri.KeywordArgumentAdapter):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._binded_kwargs: dict[str, typing.Any] = {}
-
     def process(
         self, msg: typing.Any, kwargs: collections.abc.MutableMapping[str, typing.Any]
     ) -> tuple[typing.Any, collections.abc.MutableMapping[str, typing.Any]]:

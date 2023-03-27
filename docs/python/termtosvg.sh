1d58cf8cb4a91ccd889cbb56065d820d8fe3c772
Fabien MARTY <fabien.marty@gmail.com>
2023-03-27 11:11:29 +0000

    wip

--- docs/python/termtosvg.sh
+++ docs/python/termtosvg.sh
@@ -4,18 +4,23 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}" || exit 1
 
 F=$1
+PATHPREFIX=${2:-}
+LINES=${3:-10}
+
+
 TMPDIR=termtosvg.$$
 OUTPUT=termtosvg.$$.ouput
 rm -Rf "${TMPDIR}"
-termtosvg --still-frames --screen-geometry 80x24 --template=solarized_light --command "python ${F}" "${TMPDIR}" >"${OUTPUT}" 2>&1
+termtosvg --still-frames --screen-geometry "100x${LINES}" --template=solarized_light --command "python -u ${F}" "${TMPDIR}" >"${OUTPUT}" 2>&1
 if test $? -ne 0; then
     echo "ERROR during termtosvg.sh $F, output:"
     cat "${OUTPUT}"
     exit 1
 fi
-NEWNAME="$(basename "${F}" .py).svg"
-cp -f "${TMPDIR}/termtosvg_00000.svg" "${NEWNAME}"
+NEWNAME=$(basename "${F}" .py).svg
+OLDNAME=$(ls ${TMPDIR}/*.svg |tail -1)
+cp -f "${OLDNAME}" "${NEWNAME}"
 rm -Rf "${TMPDIR}"
 rm -Rf "${OUTPUT}"
 
-echo "![rich output](docs/python/${NEWNAME})"
+echo "![rich output](${PATHPREFIX}python/${NEWNAME})"

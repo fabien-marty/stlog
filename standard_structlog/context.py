0f347b301798a798b2f1e0032219b71a0bb495ea
Fabien MARTY <fabien.marty@botify.com>
2023-04-13 14:16:46 +0000

    changes because contextvars set() does a swallow copy

--- standard_structlog/context.py
+++ standard_structlog/context.py
@@ -4,7 +4,7 @@ from contextlib import contextmanager
 from contextvars import ContextVar, Token
 from typing import Any
 
-_LOGGING_CONTEXT_VAR: ContextVar = ContextVar("logger_context", default={})
+_LOGGING_CONTEXT_VAR: ContextVar = ContextVar("stlog_logging_context", default={})
 
 
 class ExecutionContext:
@@ -15,24 +15,27 @@ class ExecutionContext:
     @classmethod
     def _add(cls, **kwargs) -> Token:
         new_context = _LOGGING_CONTEXT_VAR.get()
+        # we create a new dict here as set() does a shallow copy
         return _LOGGING_CONTEXT_VAR.set({**new_context, **kwargs})
 
     @classmethod
     def add(cls, **kwargs) -> None:
+        """Add some key / values to the execution context.
+
+        WARNING: use only immutable values in kwargs (or be sure to never change them after add() call)
+        """
         cls._add(**kwargs)
 
     @classmethod
     def remove(cls, *keys) -> None:
-        new_context = _LOGGING_CONTEXT_VAR.get()
-        for key in keys:
-            try:
-                new_context.pop(key)
-            except KeyError:
-                pass
-        _LOGGING_CONTEXT_VAR.set(new_context)
+        # we create a new dict here as set() does a shallow copy
+        _LOGGING_CONTEXT_VAR.set(
+            {k: v for k, v in _LOGGING_CONTEXT_VAR.get().items() if k not in keys}
+        )
 
     @classmethod
-    def get(cls) -> dict[str, Any]:
+    def _get(cls) -> dict[str, Any]:
+        # warning: do not modify the result dict
         return _LOGGING_CONTEXT_VAR.get()
 
     @classmethod

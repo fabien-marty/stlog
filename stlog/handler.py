e0868698f325891be68f44009b806e3b1e106dba
Fabien MARTY <fabien.marty@botify.com>
2025-05-07 13:47:36 +0200

    improve rich handler

--- stlog/handler.py
+++ stlog/handler.py
@@ -1,21 +1,54 @@
 from __future__ import annotations
 
 import logging
+import sys
 
 from stlog.base import (
-    StlogError,
     rich_dump_exception_on_console,
 )
+from stlog.formatter import HumanFormatter, RichHumanFormatter
+
+RICH_INSTALLED: bool = False
+try:
+    from rich.console import Console
+
+    RICH_INSTALLED = True
+except ImportError:
+    pass
 
 
-class CustomRichHandler(logging.Handler):
-    def __init__(self, console, level: int = logging.NOTSET, **kwargs):
-        self.console = console
-        super().__init__(level=level)
+class CustomRichHandler(logging.StreamHandler):
+    """A custom StreamHandler that uses Rich Console to print log records.
 
-    def emit(self, record: logging.LogRecord):
-        if self.formatter is None:
-            raise StlogError("no formatted set")
+    If Rich is not installed or if the stream is not a terminal,
+    this handler will fallback to the default StreamHandler.
+
+    """
+
+    def __init__(self, stream=None, **kwargs):
+        super().__init__(stream=stream)
+        self.console = None
+        self.force_terminal = kwargs.get("force_terminal", False)
+        if RICH_INSTALLED:
+            # => let's make a rich console
+            self.console = Console(
+                file=stream if stream else sys.stderr,
+                force_terminal=True if self.force_terminal else None,
+                highlight=False,
+            )
+            if not (self.force_terminal or self.console.is_terminal):
+                # Let's fallback to a basic stream handler
+                self.console = None
+        if self.console is None:
+            # Let's default to a human formatter
+            self.formatter = HumanFormatter()
+        else:
+            # Let's use a rich formatter
+            self.formatter = RichHumanFormatter()
+
+    def _rich_emit(self, record: logging.LogRecord):
+        assert self.console is not None
+        assert self.formatter is not None
         self.console.print(self.formatter.format(record))
         if record.exc_info:
             exc_type, exc_value, exc_traceback = record.exc_info
@@ -24,3 +57,9 @@ class CustomRichHandler(logging.Handler):
             rich_dump_exception_on_console(
                 self.console, exc_type, exc_value, exc_traceback
             )
+
+    def emit(self, record: logging.LogRecord):
+        if self.console is None or self.formatter is None:
+            return super().emit(record)
+        else:
+            return self._rich_emit(record)
